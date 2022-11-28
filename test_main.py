9be6cad65672a67dcd41694f3cd65014015bdc9c
Heeyong <heeyonghuh@gmail.com>
2022-11-28 12:42:37 -0600

    CCA

--- test_main.py
+++ test_main.py
@@ -12,16 +12,25 @@ import mne
 import preprocessing
 import feature_extraction
 
+# import data
 subject_num = input("Subject number: ")
 electrode_type = ['Gel', 'POLiTAG'][int(input("Electrode type (Gel [0] or POLiTAG [1]): "))]
-# session = input("Session (offline [0], online_S1 [1], online_S2 [2]): ")
+session_type = input("Session (offline [0], online_S1 [1], online_S2 [2]): ")
+if session_type == '0':
+        session = 'offline'
+elif session_type == '1':
+        session = 'online_S1'
+elif session_type == '2':
+        session = 'online_S2'
+
 raw_data_list = os.listdir(os.path.join(os.getcwd(), 'ErrP_data'))
 data_name = subject_num + '_' + electrode_type
 for data_folder in raw_data_list:
     if data_folder.startswith(data_name):
-        raw_offline_folder = os.path.join(os.getcwd(), 'ErrP_data', data_folder, 'offline')
+        raw_offline_folder = os.path.join(os.getcwd(), 'ErrP_data', data_folder, session)
         raw_run_list = os.listdir(raw_offline_folder)
 
+montage = mne.channels.make_standard_montage('standard_1020')
 raw_path_list = []
 for run in raw_run_list:
         if run.endswith('training'):
@@ -29,21 +38,32 @@ for run in raw_run_list:
 
 correct_epochs = []
 error_epochs = []
+print(raw_path_list)
+
+
+
 for file_name in raw_path_list:
         raw = mne.io.read_raw_brainvision(file_name)
 
         # raw = raw.filter(1,50)
         raw.load_data()
-        raw.set_eeg_reference('average') 
+        new_names = dict(
+                (ch_name,
+                ch_name.replace('Z', 'z').replace('FP','Fp'))
+                for ch_name in raw.ch_names)
+        raw.rename_channels(new_names)
+        raw.set_montage(montage)
+        raw.set_eeg_reference('average') # CAR
+        # fig = raw.plot_sensors(show_names=True)
 
-        filters = preprocessing.Filtering(raw, l_freq=3, h_freq=10)
+        filters = preprocessing.Filtering(raw, l_freq=5, h_freq=15)
         raw = filters.external_artifact_rejection()
         print(raw.info)
         # fig = raw.plot(block=True)
         onset = raw.annotations.onset
         duration = raw.annotations.duration
         description = raw.annotations.description
-        print(description)
+        # print(description)
         new_description = []
         for i in range(len(description)):
                 if description[i] == 'Stimulus/10':
@@ -58,15 +78,15 @@ for file_name in raw_path_list:
                         new_description.append('Create target')
                 else:
                         new_description.append(description[i])
-        print(new_description)
+        # print(new_description)
         my_annot = mne.Annotations(onset=onset, duration=duration, description=new_description)
         raw.set_annotations(my_annot)
 
         custom_dict = {'Error trial': 10, 'Correct trial & Target reached': 13, 'Correct trial': 9}
         events, event_dict = mne.events_from_annotations(raw, event_id=custom_dict)
-        print(events)
-        print(event_dict)
-        fig = mne.viz.plot_events(events, event_id=event_dict, sfreq=raw.info['sfreq'], first_samp=raw.first_samp)
+        # print(events)
+        # print(event_dict)
+        # fig = mne.viz.plot_events(events, event_id=event_dict, sfreq=raw.info['sfreq'], first_samp=raw.first_samp)
 
         epochs = mne.Epochs(raw, events, event_id=event_dict, tmin=-0.2, tmax=0.5, preload=True, picks='eeg')
         correct = epochs['Correct trial & Target reached', 'Correct trial']
@@ -76,21 +96,47 @@ for file_name in raw_path_list:
         correct_epochs.append(correct)
         error_epochs.append(error)
 all_correct = mne.concatenate_epochs(correct_epochs)
+print('len of epochs are', len(correct_epochs[0]) + len(correct_epochs[1]) + len(correct_epochs[2]))
+print('len of epochs are', len(all_correct))
+
 all_error = mne.concatenate_epochs(error_epochs)
 correct_evoked = all_correct.average()
 error_evoked = all_error.average()
-for evk in (correct_evoked, error_evoked):
-        # global field power and spatial plot
-        evk.plot(gfp=False, spatial_colors=True, ylim=dict(eeg=[-4, 4]))
+
+X_correct = np.concatenate(all_correct.get_data(),axis=1)
+X_error = np.concatenate(all_error.get_data(),axis=1)
+'''
+print(type(all_correct.get_data()), type(all_error.get_data()), type(correct_evoked.get_data()), type(error_evoked.get_data()))
+print(len(all_correct.get_data()), len(all_error.get_data()), len(correct_evoked.get_data()), len(error_evoked.get_data()))
+print(len(all_correct.get_data()))
+print(len(all_correct.get_data()[0]))
+print(len(all_correct.get_data()[0][0]))
+print(len(correct_evoked.get_data()))
+print(len(correct_evoked.get_data()[0]))
+
+print(np.concatenate(all_correct.get_data(),axis=1).shape)
+print(np.concatenate(all_correct.get_data(),axis=1)[0:4])
+print(np.tile(correct_evoked.get_data(),(1,len(all_correct.get_data()))).shape)
+print(np.tile(correct_evoked.get_data(),(1,len(all_correct.get_data())))[0:4])
+
+print(np.concatenate(all_correct.get_data(),axis=1)[0][0])
+print(np.concatenate(all_correct.get_data(),axis=1)[0][359])
+print(np.concatenate(all_correct.get_data(),axis=1)[0][718])
+print(np.tile(correct_evoked.get_data(),(1,len(all_correct.get_data())))[0][0])
+print(np.tile(correct_evoked.get_data(),(1,len(all_correct.get_data())))[0][359])
+print(np.tile(correct_evoked.get_data(),(1,len(all_correct.get_data())))[0][718])
+'''
+# for evk in (correct_evoked, error_evoked):
+#         # global field power and spatial plot
+#         evk.plot(gfp=False, spatial_colors=True, ylim=dict(eeg=[-4, 4]))
+time_unit = dict(time_unit="s")
+correct_evoked.plot_joint(title="Average Evoked for Correct", picks='eeg',ts_args=time_unit, topomap_args=time_unit)
+error_evoked.plot_joint(title="Average Evoked for ErrP", picks='eeg',ts_args=time_unit, topomap_args=time_unit)  # show difference wave
 evokeds = dict(corr=correct_evoked, err=error_evoked)
-mne.viz.plot_compare_evokeds(evokeds, picks='FZ', combine='mean')
-        # spatial plot + topomap
-        # evk.plot_joint()
-# evokeds = dict(correct_epochs=list(correct), visual=list(error))
+mne.viz.plot_compare_evokeds(evokeds, picks='Fz', combine='mean')
+mne.viz.plot_compare_evokeds(evokeds, picks=['FCz', 'FC1', 'FC2', 'Cz', 'Fz'], combine='mean')
 
-# mne.viz.plot_compare_evokeds(evokeds, legend='upper left', show_sensors='upper right')
+
 '''
 
 '''
-# times = np.arange(0.05, 0.151, 0.02)
-# evoked.plot_topomap(times, ch_type='mag')
